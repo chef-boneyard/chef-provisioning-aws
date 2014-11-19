c74baa86aa9ed8044d9b05454120d5ee657f8eda
Ryan Cragun <me@ryan.ec>
2014-11-19 14:54:01 -0800

    Add spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'chef/dsl/recipe'
+require 'chef/provisioning'
+require 'chef/provisioning/aws_driver'
+
+RSpec.configure do |rspec|
+  rspec.run_all_when_everything_filtered = true
+  rspec.filter_run :focus
+  rspec.order = 'random'
+  rspec.expect_with(:rspec) { |c| c.syntax = :expect }
+  rspec.before { allow($stdout).to receive(:write) }
+end

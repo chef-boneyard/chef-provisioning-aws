c58cf41bf76973c5c29d948fdba85633d5963802
Erik van Brakel <erik@evision-software.com>
2015-02-18 13:55:09 +0100

    Add check for existing subnet group

--- lib/chef/provider/aws_rds_db_subnet_group.rb
+++ lib/chef/provider/aws_rds_db_subnet_group.rb
@@ -4,19 +4,29 @@ class Chef::Provider::AwsRdsDbSubnetGroup < Chef::Provider::AwsProvider
 
   action :create do
 
-    options = {
-      :db_subnet_group_name => new_resource.name,
-      :db_subnet_group_description => new_resource.description,
-    }
+    if existing_db_subnet_group == nil
 
+      subnet_ids = ec2.subnets.with_tag('Name', new_resource.subnets).map { |s| s.id }
 
-    subnets_ids = ec2.subnets.with_tag('Name', new_resource.subnets).map { |s| s.id }
+      options = {
+        :db_subnet_group_name => new_resource.name,
+        :db_subnet_group_description => new_resource.description,
+        :subnet_ids => subnet_ids
+      }
+ 
+      converge_by "Creating new RDS subnet group" do
+        dbInstance = rds.client.create_db_subnet_group(options)
+      end
 
-    options[:subnet_ids] = subnets_ids;
-    converge_by "Creating new RDS subnet group" do
-      dbInstance = rds.client.create_db_subnet_group(options)
     end
+  end
 
+  def existing_db_subnet_group
+    @existing_db_subnet_group ||= begin
+      rds.client.describe_db_subnet_groups(db_subnet_group_name: new_resource.name).first
+    rescue
+      nil
+    end
   end
 
 end

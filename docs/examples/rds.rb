5c26a3b2fdbc0c92baa95ed74f8b4e33f47e0279
Erik van Brakel <erik@evision-software.com>
2015-03-01 15:35:37 +0100

    Fix example

--- docs/examples/rds.rb
+++ docs/examples/rds.rb
@@ -1,7 +1,7 @@
 require 'chef/provisioning/aws_driver'
 with_driver 'aws'
 
-with_data_center 'eu-west-1' do
+with_driver 'aws::eu-west-1' do
   aws_vpc "provisioning-vpc" do 
     cidr_block "10.0.0.0/16"
   end

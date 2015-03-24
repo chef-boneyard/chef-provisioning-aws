e6233a684ef1b8acc93a7e876ca156e543ed6ebe
Patrick Wright <patrick@getchef.com>
2015-03-23 17:43:07 -0700

    aws_ebs_volume: optimize detach, add Errors

--- lib/chef/provider/aws_ebs_volume.rb
+++ lib/chef/provider/aws_ebs_volume.rb
@@ -4,6 +4,24 @@ require 'date'
 require 'retryable'
 
 class Chef::Provider::AwsEbsVolume < Chef::Provisioning::AWSDriver::AWSProvider
+  class VolumeNotFoundError < RuntimeError
+    def initialize(new_resource)
+      super("#{new_resource} does not exist!")
+    end
+  end
+
+  class VolumeStatusTimeoutError < TimeoutError
+    def initialize(new_resource, initial_status, expected_status)
+      super("timed out waiting for #{new_resource} status to change from #{initial_status} to #{expected_status}!")
+    end
+  end
+
+  class VolumeInvalidStatusError < RuntimeError
+    def initialize(new_resource, status)
+      super("#{new_resource} is in #{status} state!")
+    end
+  end
+
   def action_create
     volume = super
 
@@ -16,77 +34,70 @@ class Chef::Provider::AwsEbsVolume < Chef::Provisioning::AWSDriver::AWSProvider
 
   def create_aws_object
     volume = nil
-    converge_by "create new EBS volume #{new_resource.name} in #{region}" do
-      volume = new_resource.driver.ec2.volumes.create(desired_options)
+    converge_by "create new #{new_resource} in #{region}" do
+      volume = new_resource.driver.ec2.volumes.create(initial_options)
       volume.tags['Name'] = new_resource.name
     end
 
-    converge_by "wait for new EBS volume #{new_resource.name} in #{region} to become available" do
+    converge_by "wait for new #{new_resource} in #{region} to become available" do
       wait_for_volume_status(volume, :available)
       volume
     end
   end
 
   def update_aws_object(volume)
-    if desired_options.has_key?(:availability_zone)
-      if desired_options[:availability_zone] != volume.availability_zone_name
-        raise "#{new_resource.to_s}.availability_zone is #{new_resource.availability_zone}, but actual volume has availability_zone_name set to #{volume.availability_zone_name}.  Cannot be modified!"
+    if initial_options.has_key?(:availability_zone)
+      if initial_options[:availability_zone] != volume.availability_zone_name
+        raise "#{new_resource}.availability_zone is #{new_resource.availability_zone}, but actual volume has availability_zone_name set to #{volume.availability_zone_name}.  Cannot be modified!"
       end
     end
-    if desired_options.has_key?(:size)
-      if desired_options[:size] != volume.size
-        raise "#{new_resource.to_s}.size is #{new_resource.size}, but actual volume has size set to #{volume.size}.  Cannot be modified!"
+    if initial_options.has_key?(:size)
+      if initial_options[:size] != volume.size
+        raise "#{new_resource}.size is #{new_resource.size}, but actual volume has size set to #{volume.size}.  Cannot be modified!"
       end
     end
-    if desired_options.has_key?(:snapshot)
-      if desired_options[:snapshot] != snapshot.id
-        raise "#{new_resource.to_s}.snapshot is #{new_resource.snapshot}, but actual volume has snapshot set to #{volume.snapshot.id}.  Cannot be modified!"
+    if initial_options.has_key?(:snapshot)
+      if initial_options[:snapshot] != volume.snapshot.id
+        raise "#{new_resource}.snapshot is #{new_resource.snapshot}, but actual volume has snapshot set to #{volume.snapshot.id}.  Cannot be modified!"
       end
     end
-    if desired_options.has_key?(:iops)
-      if desired_options[:iops] != volume.iops
-        raise "#{new_resource.to_s}.iops is #{new_resource.iops}, but actual volume has iops set to #{volume.iops}.  Cannot be modified!"
+    if initial_options.has_key?(:iops)
+      if initial_options[:iops] != volume.iops
+        raise "#{new_resource}.iops is #{new_resource.iops}, but actual volume has iops set to #{volume.iops}.  Cannot be modified!"
       end
     end
-    if desired_options.has_key?(:volume_type)
-      if desired_options[:volume_type] != volume.type
-        raise "#{new_resource.to_s}.volume_type is #{new_resource.volume_type}, but actual volume has type set to #{volume.type}.  Cannot be modified!"
+    if initial_options.has_key?(:volume_type)
+      if initial_options[:volume_type] != volume.type
+        raise "#{new_resource}.volume_type is #{new_resource.volume_type}, but actual volume has type set to #{volume.type}.  Cannot be modified!"
       end
     end
-    if desired_options.has_key?(:encrypted)
-      if desired_options[:encrypted] != !!volume.encrypted
-        raise "#{new_resource.to_s}.encrypted is #{new_resource.encrypted}, but actual volume has encrypted set to #{volume.encrypted}.  Cannot be modified!"
+    if initial_options.has_key?(:encrypted)
+      if initial_options[:encrypted] != !!volume.encrypted
+        raise "#{new_resource}.encrypted is #{new_resource.encrypted}, but actual volume has encrypted set to #{volume.encrypted}.  Cannot be modified!"
       end
     end
   end
 
   def destroy_aws_object(volume)
-    status = volume.status
-    case status
-    when :in_use
-      attachment = volume.attachments.first
-      detach(volume, :instance => attachment.instance, :device => attachment.device)
-      delete(volume)
-    else
-      delete(volume)
-    end
+    detach(volume) if volume.status == :in_use
+    delete(volume)
   end
 
   private
 
-  def desired_instance
-    if !defined?(@desired_instance)
+  def expected_instance
+    if !defined?(@expected_instance)
       if new_resource.machine == false
-        @desired_instance = nil
+        @expected_instance = nil
       else
-        @desired_instance = Chef::Resource::AwsInstance.get_aws_object(new_resource.machine, resource: new_resource)
+        @expected_instance = Chef::Resource::AwsInstance.get_aws_object(new_resource.machine, resource: new_resource)
       end
     end
-    @desired_instance
+    @expected_instance
   end
 
-  def desired_options
-    @desired_options ||= begin
+  def initial_options
+    @initial_options ||= begin
       options = {}
       options[:availability_zone] = new_resource.availability_zone if !new_resource.availability_zone.nil?
       options[:size]              = new_resource.size              if !new_resource.size.nil?
@@ -105,26 +116,17 @@ class Chef::Provider::AwsEbsVolume < Chef::Provisioning::AWSDriver::AWSProvider
     #
     # If we were told to attach the volume to a machine, do so
     #
-    if desired_instance.is_a?(AWS::EC2::Instance)
+    if expected_instance.is_a?(AWS::EC2::Instance)
       case status
       when :in_use
-        attachment = volume.attachments.first
-        # wrong instance
-        if attachment.instance != desired_instance
-          detach(volume, :instance => attachment.instance, :device => attachment.device)
-          attach(volume)
-        # wrong device only
-        elsif attachment.instance == desired_instance && attachment.device != new_resource.device
-          detach(volume, :device => attachment.device)
-          attach(volume)
-        end
-
+        detach(volume)
+        attach(volume)
       when :available
         attach(volume)
       when nil
-        raise "EBS volume #{new_resource.name} does not currently exist!"
+        raise VolumeNotFoundError.new(new_resource)
       else
-        raise "EBS volume #{new_resource.name} (#{volume.id}) is in #{status} state!"
+        raise VolumeInvalidStatusError.new(new_resource, status)
       end
 
     #
@@ -133,64 +135,64 @@ class Chef::Provider::AwsEbsVolume < Chef::Provisioning::AWSDriver::AWSProvider
     else
       case status
       when nil
-        Chef::Log.warn "EBS volume #{new_resource.name} does not currently exist!"
+        Chef::Log.warn VolumeNotFoundError.new(new_resource)
       when :in_use
-        attachment = volume.attachments.first
-        detach(volume, :instance => attachment.instance, :device => attachment.device)
+        detach(volume)
       end
     end
     volume
   end
     
-  def wait_for_volume_status(volume, status)
+  def wait_for_volume_status(volume, expected_status)
+    initial_status = volume.status
     log_callback = proc {
-      Chef::Log.info("waiting for volume status to change to #{status}...")
+      Chef::Log.info("waiting for #{new_resource} status to change to #{expected_status}...")
     }
 
-    Retryable.retryable(:tries => 30, :sleep => 2, :on => TimeoutError, :ensure => log_callback) do
-      raise TimeoutError,
-        "timed out waiting for volume status to change from #{volume.status} to #{status}!" if volume.status != status
+    Retryable.retryable(:tries => 30, :sleep => 2, :on => VolumeStatusTimeoutError, :ensure => log_callback) do
+      raise VolumeStatusTimeoutError.new(new_resource, initial_status, expected_status) if volume.status != expected_status
     end
   end
 
-  def detach(volume, options = {})
-    instance = options[:instance] || desired_instance
-    device   = options[:device] || volume.attachments.first.device
+  def detach(volume)
+    instance = volume.attachments.first.instance
+    device   = volume.attachments.first.device
 
-    converge_by "detach EBS volume #{new_resource.name} in #{region} from instance #{new_resource.machine} (#{instance.instance_id})" do
+    converge_by "detach #{new_resource} from #{new_resource.machine} (#{instance.instance_id})" do
       volume.detach_from(instance, device)
     end
 
-    converge_by "wait for EBS volume #{new_resource.name} in #{region} to detach" do
+    converge_by "wait for #{new_resource} to detach" do
       wait_for_volume_status(volume, :available)
+      volume
     end
   end
 
   def attach(volume)
-    converge_by "attach EBS volume #{new_resource.name} in #{region} to instance #{new_resource.machine} (#{desired_instance.instance_id})" do
-      volume.attach_to(desired_instance, new_resource.device)
+    converge_by "attach #{new_resource} to #{new_resource.machine} (#{expected_instance.instance_id})" do
+      volume.attach_to(expected_instance, new_resource.device)
     end
 
-    converge_by "wait for EBS volume #{new_resource.name} in #{region} to attach" do
+    converge_by "wait for #{new_resource} to attach" do
       wait_for_volume_status(volume, :in_use)
       volume
     end
   end
 
   def delete(volume)
-    converge_by "delete EBS volume #{new_resource.name} in #{region}" do
+    converge_by "delete #{new_resource} in #{region}" do
       volume.delete
     end
 
-    converge_by "wait for EBS volume #{new_resource.name} in #{region} to delete" do
+    converge_by "wait for #{new_resource} in #{region} to delete" do
       log_callback = proc {
         Chef::Log.info('waiting for volume to delete...')
       }
 
-      Retryable.retryable(:tries => 30, :sleep => 2, :on => TimeoutError, :ensure => log_callback) do
-        raise TimeoutError,
-          "timed out waiting for EBS volume #{new_resource.name} (#{volume.id}) to delete!" if volume.exists?
+      Retryable.retryable(:tries => 30, :sleep => 2, :on => VolumeStatusTimeoutError, :ensure => log_callback) do
+        raise VolumeStatusTimeoutError.new(new_resource, 'exists', 'deleted') if volume.exists?
       end
+      volume
     end
   end
 end

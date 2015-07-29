7c3a5edb25cc9c1882de22fe7bb38f1b318df3f5
tyler-ball <tyleraball@gmail.com>
2015-07-29 09:51:29 -0600

    Merged an error from https://github.com/chef/chef-provisioning-aws/pull/263, fixing it

--- lib/chef/provider/aws_network_interface.rb
+++ lib/chef/provider/aws_network_interface.rb
@@ -35,7 +35,7 @@ class Chef::Provider::AwsNetworkInterface < Chef::Provisioning::AWSDriver::AWSPr
   def create_aws_object
     eni = nil
     converge_by "create new #{new_resource} in #{region}" do
-      eni = new_resource9.driver.ec2.network_interfaces.create(options)
+      eni = new_resource.driver.ec2.network_interfaces.create(options)
       retry_with_backoff(AWS::EC2::Errors::InvalidNetworkInterfaceID::NotFound) do
         eni.tags['Name'] = new_resource.name
       end

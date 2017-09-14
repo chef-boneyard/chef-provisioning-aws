68b173656d5a24b4a2982543f428b3fbe8597d96
Harikesh Kolekar <harikesh.kolekar@msystechnologies.com>
2017-09-14 18:08:22 +0530

    skip integration specs
    Signed-off-by: Harikesh Kolekar <harikesh.kolekar@msystechnologies.com>

--- tools/travis_env.sh
+++ tools/travis_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "${TRAVIS_SECURE_ENV_VARS}" == "true" ]; then
-  export AWS_TEST_DRIVER=$AWS_TRAVIS_DRIVER
+  #export AWS_TEST_DRIVER=$AWS_TRAVIS_DRIVER
   echo "AWS_TEST_DRIVER set to ${AWS_TEST_DRIVER}"
 else
   unset AWS_TEST_DRIVER
